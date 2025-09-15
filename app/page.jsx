97c166b92864dca92def251843fc786e89ba3049
Nick <strangewin@gmail.com>
2025-09-15 14:15:35 -0500

    home page touchups

--- app/page.jsx
+++ app/page.jsx
@@ -32,13 +32,15 @@ export default function Home() {
       );
     }
   }, []);
-
-  const handleLeftHover = () => {
-    gsap.to(centerTextDesktopRef.current, {
-      x: glideDistance,
-      duration: 0.8,
-      ease: "power2.inOut",
-    });
+const handleLeftHover = () => {
+  gsap.to(centerTextDesktopRef.current, {
+    x: glideDistance,
+    duration: 0.8,
+    ease: "power2.inOut",
+    onStart: () => {
+      centerTextDesktopRef.current.style.textAlign = "right";
+    },
+  });
     gsap.to(rightRectRef.current, {
       opacity: 0,
       duration: 0.5,
@@ -61,6 +63,9 @@ export default function Home() {
       x: 0,
       duration: 0.8,
       ease: "power2.inOut",
+      onStart: () => {
+        centerTextDesktopRef.current.style.textAlign = "center";
+      }
     });
     gsap.to(rightRectRef.current, {
       opacity: 1,
@@ -77,6 +82,7 @@ export default function Home() {
       duration: 0.5,
       ease: "power2.inOut",
     });
+    
   };
 
   const handleRightHover = () => {
@@ -84,6 +90,9 @@ export default function Home() {
       x: -glideDistance,
       duration: 0.8,
       ease: "power2.inOut",
+      onStart: () => {
+        centerTextDesktopRef.current.style.textAlign = "left";
+      }
     });
     gsap.to(leftRectRef.current, {
       opacity: 0,
@@ -107,6 +116,9 @@ export default function Home() {
       x: 0,
       duration: 0.8,
       ease: "power2.inOut",
+      onStart: () => {
+        centerTextDesktopRef.current.style.textAlign = "center";
+      }
     });
     gsap.to(leftRectRef.current, {
       opacity: 1,
@@ -128,118 +140,121 @@ export default function Home() {
   return (
     <div className="relative min-h-screen w-full bg-white overflow-hidden">
       {/* DESKTOP LAYOUT (>=1024px) */}
-      <div className="hidden lg:block">
-        <div
-          ref={leftRectRef}
-          className="absolute border-2 border-dashed opacity-100 pointer-events-none"
-          style={{
-            width: 502,
-            height: 502,
-            top: 319,
-            left: -301,
-            borderColor: "#bbb",
-            transform: "rotate(45deg)",
-            zIndex: 5,
-          }}
-        />
-        <div
-          ref={rightRectRef}
-          className="absolute border-2 border-dashed opacity-100 pointer-events-none"
-          style={{
-            width: 502,
-            height: 502,
-            top: 319,
-            right: -301,
-            borderColor: "#bbb",
-            transform: "rotate(-45deg)",
-            zIndex: 5,
-          }}
-        />
+     <div className="hidden lg:flex flex-col items-center justify-center min-h-screen relative">
+  {/* Left Rect */}
+  <div
+    ref={leftRectRef}
+    className="absolute border-2 border-dashed pointer-events-none"
+    style={{
+      width: 502,
+      height: 502,
+      top: "50%",
+      left: "-250px",
+      transform: "translateY(-50%) rotate(45deg)",
+      borderColor: "#bbb",
+      zIndex: 5,
+    }}
+  />
+  {/* Right Rect */}
+  <div
+    ref={rightRectRef}
+    className="absolute border-2 border-dashed pointer-events-none"
+    style={{
+      width: 502,
+      height: 502,
+      top: "50%",
+      right: "-250px",
+      transform: "translateY(-50%) rotate(-45deg)",
+      borderColor: "#bbb",
+      zIndex: 5,
+    }}
+  />
 
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <h1
-            ref={centerTextDesktopRef}
-            className="z-10 text-center"
-            style={{
-              fontFamily: "Roobert TRIAL, sans-serif",
-              fontWeight: 300,
-              fontSize: "100px",
-              lineHeight: "120px",
-              letterSpacing: "-0.07em",
-            }}
-          >
-            Sophisticated
-            <br />
-            skincare
-          </h1>
-        </div>
+  {/* Center text */}
+  <h1
+    ref={centerTextDesktopRef}
+    className="z-10 text-center px-4"
+    style={{
+      fontFamily: "Roobert TRIAL, sans-serif",
+      fontWeight: 300,
+      fontSize: "clamp(48px, 8vw, 100px)", 
+      lineHeight: "1.1",
+      letterSpacing: "-0.07em",
+    }}
+  >
+    Sophisticated
+    <br />
+    skincare
+  </h1>
 
-        <div
-          ref={leftArrowRef}
-          onMouseEnter={handleLeftHover}
-          onMouseLeave={handleLeftHoverOut}
-          className="absolute z-30 flex items-center"
-          style={{
-            width: "max-content",
-            left: 100,
-            top: 560,
-            transform: "translateY(-50%)",
-          }}
-        >
-          <Link href="/" className="flex items-center group">
-            <span className="inline-flex items-center justify-center w-7 h-7 border border-black rotate-45 mr-2">
-              <IoTriangleSharp className="-rotate-20" size={12} />
-            </span>
-            <span className="text-xs text-black group-hover: whitespace-nowrap">
-              DISCOVER A.I.
-            </span>
-          </Link>
-        </div>
-
-        <div
-          ref={rightArrowRef}
-          onMouseEnter={handleRightHover}
-          onMouseLeave={handleRightHoverOut}
-          className="absolute z-30 flex items-center"
-          style={{
-            width: "max-content",
-            right: 100,
-            top: 560,
-            transform: "translateY(-50%)",
-          }}
-        >
-          <Link href="/testing" className="flex items-center group">
-            <span className="text-xs text-black group-hover:whitespace-nowrap mr-2">
-              TAKE TEST
-            </span>
-            <span className="inline-flex items-center justify-center w-7 h-7 border border-black rotate-45">
-              <IoTriangleSharp className="-rotate-320" size={12} />
-            </span>
-          </Link>
-        </div>
-
-  <div className="absolute left-4 sm:left-8 lg:left-12 bottom-12 sm:bottom-16 lg:bottom-24 max-w-[90vw] sm:max-w-sm lg:max-w-md px-2">
-  <p className="
-    text-black opacity-80 font-Roobert TRIAL leading-snug
-    text-[10px] sm:text-xs lg:text-sm
-    font-medium sm:font-semibold lg:font-bold
-  ">
-    <span className="hidden lg:inline">
-      SKINSTRIC DEVELOPED AN A.I. THAT CREATES A<br />
-      HIGHLY-PERSONALIZED ROUTINE TAILORED TO<br />
-      WHAT YOUR SKIN NEEDS.
+ {/* LEFT ARROW */}
+<div
+  ref={leftArrowRef}
+  onMouseEnter={handleLeftHover}
+  onMouseLeave={handleLeftHoverOut}
+  className="absolute z-30 flex items-center"
+  style={{
+    width: "max-content",
+    left: "10%",       
+    top: "50vh",      
+    transform: "translateY(-50%)",
+  }}
+>
+  <Link href="/" className="flex items-center group">
+    <span className="inline-flex items-center justify-center w-7 h-7 border border-black rotate-45 mr-2">
+      <IoTriangleSharp className="-rotate-20" size={12} />
     </span>
-    <span className="hidden sm:inline lg:hidden">
-      SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALIZED ROUTINE
-      TAILORED TO WHAT YOUR SKIN NEEDS.
+    <span className="text-xs text-black group-hover: whitespace-nowrap">
+      DISCOVER A.I.
     </span>
-    <span className="inline sm:hidden">
-      SKINSTRIC: A.I. for your personalized skincare routine.
-    </span>
-  </p>
+  </Link>
 </div>
 
-      </div>
+{/* RIGHT ARROW */}
+<div
+  ref={rightArrowRef}
+  onMouseEnter={handleRightHover}
+  onMouseLeave={handleRightHoverOut}
+  className="absolute z-30 flex items-center"
+  style={{
+    width: "max-content",
+    right: "10%",     
+    top: "50vh",      
+    transform: "translateY(-50%)",
+  }}
+>
+  <Link href="/testing" className="flex items-center group">
+    <span className="text-xs text-black group-hover:whitespace-nowrap mr-2">
+      TAKE TEST
+    </span>
+    <span className="inline-flex items-center justify-center w-7 h-7 border border-black rotate-45">
+      <IoTriangleSharp className="-rotate-320" size={12} />
+    </span>
+  </Link>
+</div>
+
+  {/* Bottom tagline */}
+  <div className="absolute left-4 sm:left-8 lg:left-12 bottom-[5vh] max-w-[90vw] sm:max-w-sm lg:max-w-md px-2">
+    <p className="
+      text-black opacity-80 font-Roobert TRIAL leading-snug
+      text-[10px] sm:text-xs lg:text-sm
+      font-medium sm:font-semibold lg:font-bold
+    ">
+      <span className="hidden lg:inline">
+        SKINSTRIC DEVELOPED AN A.I. THAT CREATES A<br />
+        HIGHLY-PERSONALIZED ROUTINE TAILORED TO<br />
+        WHAT YOUR SKIN NEEDS.
+      </span>
+      <span className="hidden sm:inline lg:hidden">
+        SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALIZED ROUTINE
+        TAILORED TO WHAT YOUR SKIN NEEDS.
+      </span>
+      <span className="inline sm:hidden">
+        SKINSTRIC: A.I. for your personalized skincare routine.
+      </span>
+    </p>
+  </div>
+</div>
 
       {/* MOBILE LAYOUT (<1024px) */}
       <div className="lg:hidden flex flex-col items-center justify-center min-h-screen relative">
